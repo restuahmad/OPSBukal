098ea0c39a647bf08e2548a1098cdb6eac13413d
restuahmad <33564290+restuahmad@users.noreply.github.com>
2017-11-12 23:45:25 +0800

    Update safelink5.js

--- safelink5.js
+++ safelink5.js
@@ -18,7 +18,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href=" https://safelink-opsbukal.blogspot.com/2017/11/arctic-melt-season-changes-and-arctic.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://safelink-opsbukal.blogspot.com/2017/11/7-terrifying-playgrounds-in-world.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
