d9f1831046b0967937f9134a02daec5021e61395
restuahmad <33564290+restuahmad@users.noreply.github.com>
2017-11-14 05:36:11 +0800

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -26,7 +26,8 @@ if(x=="1"){xxx="/2017/11/car-production-fall-hits-uk-industrial.html"}
 if(x=="2"){xxx="/2017/11/tesla-car-battery-production-releases.html"}
 if(x=="3"){xxx="/2017/11/7-terrifying-playgrounds-in-world.html"}
 if(x=="4"){xxx="/2017/11/us-climate-report-edits-out-highly.html"}
-if(x=="5"){xxx="/2017/11/arctic-melt-season-changes-and-arctic.html"}a_to_ve[i].href="https://safelink-opsbukal.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/11/arctic-melt-season-changes-and-arctic.html"}
+if(x=="6"){xxx="/2017/12/testi.html"}a_to_ve[i].href="https://safelink-opsbukal.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
