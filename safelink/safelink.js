284f162e9e30e0b2d4641b5fe03c82045924336b
restuahmad <33564290+restuahmad@users.noreply.github.com>
2021-06-01 18:46:17 +0800

    Update safelink.js

--- safelink/safelink.js
+++ safelink/safelink.js
@@ -23,8 +23,9 @@ var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="the-needed-goals-of-quality-public.html"}
 if(x=="2"){xxx="8-simple-ways-to-save-money.html"}
-if(x=="3"){xxx="advantages-of-being-home-business-owner.html"}
-if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="https://link.opsbukal.my.id/2021/05/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="budgeting-in-your-20s-how-to-get-started.html"}
+if(x=="4"){xxx="12-tricks-to-save-money-on-baby-gear.html"}
+if(x=="5"){xxx="why-financial-planning-is-important-for.html"}a_to_ve[i].href="https://link.opsbukal.my.id/2021/05/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
