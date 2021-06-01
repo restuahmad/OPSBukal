b94e01d18bbf33a0e00523ff121cb8b5cc0ac93f
restuahmad <33564290+restuahmad@users.noreply.github.com>
2021-06-01 18:12:02 +0800

    Update safelink.js
    update link 2

--- safelink/safelink.js
+++ safelink/safelink.js
@@ -22,7 +22,7 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="the-needed-goals-of-quality-public.html"}
-if(x=="2"){xxx="10-tips-for-start-small-business-startup.html"}
+if(x=="2"){xxx="8-simple-ways-to-save-money.html"}
 if(x=="3"){xxx="advantages-of-being-home-business-owner.html"}
 if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="https://link.opsbukal.my.id/2021/05/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
