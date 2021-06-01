7f11ef7533dddf050ef5af9f4a092633f43f355c
restuahmad <33564290+restuahmad@users.noreply.github.com>
2021-06-01 22:00:49 +0800

    Update safelink_utama.js

--- safelink/safelink_utama.js
+++ safelink/safelink_utama.js
@@ -19,7 +19,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
+var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="the-needed-goals-of-quality-public.html"}
 if(x=="2"){xxx="8-simple-ways-to-save-money.html"}
